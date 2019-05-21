a601a0ea068f5c0768961224242d16255c8df13f
Calvin <calvinshum1874@gmail.com>
2019-05-20 17:05:47 -0700

    trying to push to heroku

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const path = require('path')
 const router = require('../Database/router')
 // const menus = require('../database/models.js')
